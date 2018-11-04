03184cf3454e568d00dcbfc77290e9b21e5b7548
yusu24 <yusuf24ef@gmail.com>
2018-11-04 20:33:59 +0700

    Add files via upload

---
+++ jadwalkuliah.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: M Yusuf Effendi
+ * Date: 25/10/2018
+ * Time: 17.29
+ */?>
+
+
+<div class="container">
+    <a href="index.html">
+    <h3>Page Dummy</h3>
+</div>
